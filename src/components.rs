d5e285afec0ccf832023367cf2a281594ec69b70
Marko Grizelj <marko.grizelj@gauss.dev>
2024-06-21 15:36:35 +0200

    chore: stat boost

--- src/components.rs
+++ src/components.rs
@@ -3,9 +3,11 @@ mod item_select;
 mod move_select;
 mod navbar;
 mod number_field;
+mod stat_boost;
 
 pub use digivolution_select::DigivolutionSelect;
 pub use item_select::ItemSelect;
 pub use move_select::MoveSelect;
 pub use navbar::Navbar;
 pub use number_field::NumberField;
+pub use stat_boost::StatBoost;
