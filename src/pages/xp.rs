093b9b1e24f2eb7eedd36df9fe76651397430239
Marko Grizelj <marko.grizelj@gauss.dev>
2024-06-25 14:22:32 +0200

    feat: more xp logic

--- src/pages/xp.rs
+++ src/pages/xp.rs
@@ -5,8 +5,12 @@ use crate::components;
 #[component]
 pub fn XP() -> Element {
     let mut exp = use_signal(|| 0);
+    let level: Signal<i64> = use_signal(|| 1);
+    let mut target_level = use_signal(|| 2);
 
     let c_exp = exp();
+    let c_level = level();
+    let c_target_level = target_level();
 
     rsx! {
         div {
@@ -29,6 +33,23 @@ pub fn XP() -> Element {
                         }
                     }
                 }
+            },
+            div {
+                class: "column",
+                div {
+                    class: "container",
+                    "Current level: {c_level}"
+                    components::NumberField {
+                        label: "Target level",
+                        value: c_target_level,
+                        mn: c_level + 1,
+                        mx: 99,
+                        disabled: false,
+                        cb: move |x: i64| {
+                            target_level.set(x);
+                        }
+                    }
+                },
             }
         }
     }
