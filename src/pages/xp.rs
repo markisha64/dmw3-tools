f02e589ecca240bc9d611e47ee6b10f79bb107b4
Marko Grizelj <marko.grizelj@gauss.dev>
2024-06-25 11:21:44 +0200

    feat: more xp

--- src/pages/xp.rs
+++ src/pages/xp.rs
@@ -4,9 +4,32 @@ use crate::components;
 
 #[component]
 pub fn XP() -> Element {
+    let mut exp = use_signal(|| 0);
+
+    let c_exp = exp();
+
     rsx! {
-        components::RookieSelect {
-            onchange: |_| { }
+        div {
+            class: "row",
+            div {
+                class: "column",
+                div {
+                    class: "container",
+                    components::RookieSelect {
+                        onchange: |_| { }
+                    },
+                    components::NumberField {
+                        disabled: false,
+                        label: "Exp",
+                        value: c_exp,
+                        mn: 0,
+                        mx: 99999999,
+                        cb: move |new_exp| {
+                          exp.set(new_exp);
+                        }
+                    }
+                }
+            }
         }
     }
 }
