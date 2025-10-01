28dddd88dff76ae20356b2254ff96c45b6014b46
markisha64 <marko.grizelj03@gmail.com>
2025-10-01 20:39:51 +0200

    feat: other cb

--- src/pages/map_entities.rs
+++ src/pages/map_entities.rs
@@ -141,6 +141,7 @@ fn scriptToString(script: ScriptConditionStep, item_names: &Vec<String>) -> Stri
         64 => format!("{} flag story #{}", set_s, value),
         116 => format!("Start scripted battle #{}", value),
         118 => format!("Start card battle #{}", value),
+        120 => format!("Start stronger card battle #{}", value),
         128..=143 => {
             let add_s = match script.flag {
                 0 => "Remove",
